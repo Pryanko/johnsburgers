43e77e55868e7ba479315a165e64199a107d7f44
libgo <pryamov.g@gmail.com>
2017-12-03 01:44:55 +0300

    Наладил взаимосвязь BottomBar и фрагментов

--- app/src/main/java/com/examle/libgo/johnsburgers/presentation/activities/HeadActivity.java
+++ app/src/main/java/com/examle/libgo/johnsburgers/presentation/activities/HeadActivity.java
@@ -1,19 +1,23 @@
 package com.examle.libgo.johnsburgers.presentation.activities;
 
-
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.examle.libgo.johnsburgers.R;
+import com.examle.libgo.johnsburgers.presentation.fragments.BasketFragment;
+import com.examle.libgo.johnsburgers.presentation.fragments.InfoFragment;
+import com.examle.libgo.johnsburgers.presentation.fragments.MenuFragment;
 import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HeadActivity extends MvpAppCompatActivity {
-
+    //Bind View
     @BindView(R.id.app_toolbar)
     Toolbar toolbar;
     @BindView(R.id.textToolbar)
@@ -21,16 +25,61 @@ public class HeadActivity extends MvpAppCompatActivity {
     @BindView(R.id.bottomBar)
     BottomBar bottomBar;
 
+    //Fragments
+    InfoFragment infoFragment;
+    MenuFragment menuFragment;
+    BasketFragment basketFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_head);
         ButterKnife.bind(this);
+        initFragment();
+        tabSelectListener();
 
         if (savedInstanceState == null){
-
+            replaceFragment(infoFragment);
         }
-
     }
+
+    private void tabSelectListener(){
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+              switch (tabId){
+                  case R.id.tab_burger_info:
+                      replaceFragment(infoFragment);
+                      break;
+                  case R.id.tab_menu:
+                      replaceFragment(menuFragment);
+                      break;
+                  case R.id.tab_basket:
+                      replaceFragment(basketFragment);
+                      break;
+              }
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container_fragments, fragment)
+                .commit();
+    }
+
+    protected void initFragment(){
+        if (infoFragment == null) {
+            infoFragment = new InfoFragment();
+        }
+        if (menuFragment == null) {
+            menuFragment = new MenuFragment();
+        }
+        if (basketFragment == null) {
+            basketFragment = new BasketFragment();
+        }
+    }
+
+
 }
